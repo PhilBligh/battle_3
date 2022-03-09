ba661ebc3dddbc118da9e1d9f1bb9505c53ff1cd
PhilBligh <philbligh@hotmail.com>
2022-03-09 16:52:07 +0000

    enabled sessions and redirection
    Co-authored-by: James Mooney <jam9304@users.noreply.github.com>

--- spec/features/testing_infrastructure_spec.rb
+++ spec/features/testing_infrastructure_spec.rb
@@ -1,8 +1,8 @@
 feature 'Testing player names' do
   scenario 'Can add names via form and see them' do
     visit('/')
-    fill_in 'Player 1', with: 'James'
-    fill_in 'Player 2', with: 'Phil'
+    fill_in 'player_1', with: 'James'
+    fill_in 'player_2', with: 'Phil'
     click_button('Submit')
     expect(page).to have_content 'James Vs Phil'
   end
