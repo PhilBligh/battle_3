697eb26c0c22d2aaf882df38f048612da4e0d93f
James Mooney <jmooney9304@gmail.com>
2022-03-09 17:11:21 +0000

    Added HP
    Co-authored-by: Phil Bligh <PhilBligh@users.noreply.github.com>

--- spec/features/testing_infrastructure_spec.rb
+++ spec/features/testing_infrastructure_spec.rb
@@ -1,4 +1,4 @@
-feature 'Testing player names' do
+feature 'Testing players' do
   scenario 'Can add names via form and see them' do
     visit('/')
     fill_in 'player_1', with: 'James'
@@ -6,4 +6,12 @@ feature 'Testing player names' do
     click_button('Submit')
     expect(page).to have_content 'James Vs Phil'
   end
+  scenario 'Will hit points after submitting names' do
+    visit('/')
+    fill_in 'player_1', with: 'James'
+    fill_in 'player_2', with: 'Phil'
+    click_button('Submit')
+    expect(page).to have_content 'Phil: 100HP'
+  end
+
 end
